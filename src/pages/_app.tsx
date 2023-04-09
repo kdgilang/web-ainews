a81c06078d860622692d167b4e73c5bfc51a0d43
gilang <kadekgilangputra@gmail.com>
2023-04-09 09:51:40 +0800

    add progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import BaseContext from '@src/contexts/BaseContext'
+import NextNProgress from 'nextjs-progressbar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <BaseContext>
+      <NextNProgress color="#1b8415" startPosition={0.3} stopDelayMs={200} height={3} showOnShallow={true} />
       <Component {...pageProps} />
     </BaseContext>
   )
