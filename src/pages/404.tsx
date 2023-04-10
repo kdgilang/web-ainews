daca9300d1a72f13ffbeefad5c662414c22ff422
gilang <kadekgilangputra@gmail.com>
2023-04-10 10:25:14 +0800

    add 404 page close #14 (#22)
    * add 404 page close #14
    
    * update 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <>
+      <Head>
+        <title>AI News | 404</title>
+        <meta name="description" content="Page not found" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen w-full flex flex-col justify-center items-center bg-white dark:bg-slate-900">
+        <h1 className="text-9xl font-extrabold text-slate-900 dark:text-white tracking-widest">404</h1>
+        <div className="uppercase font-bold bg-green px-2 py-1 text-sm rounded rotate-12 absolute text-white text-xs">
+          Page Not Found
+        </div>
+        <button className="mt-5">
+          <a
+            className="relative rounded inline-block text-sm font-medium text-slate-900 dark:text-white group hover:text-green hover:dark:text-green focus:outline-none focus:ring"
+          >
+            <span className="relative block px-8 py-3 border border-current rounded">
+              <Link href="/">Go Home</Link>
+            </span>
+          </a>
+        </button>
+      </div>
+    </>
+  )
+}
