aa0a23a1617cfc2b5fddba6d9be1d5163ebac1cb
gilang <kadekgilangputra@gmail.com>
2023-04-13 09:47:18 +0800

    add sharp and optimize config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
+  removeConsole: process.env.NODE_ENV === "production",
   images: {
     domains: ['i.guim.co.uk', 'www.si.com', 'img.tfd.com', 'cdn.mefi.us', 's.yimg.com'],
     remotePatterns: [
