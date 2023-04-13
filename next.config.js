e2c2e16fa65b33b6c1e0dbef55c2fc2af429502a
gilang <kadekgilangputra@gmail.com>
2023-04-13 10:27:10 +0800

    improve site

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
-  removeConsole: process.env.NODE_ENV === "production",
   images: {
     domains: ['i.guim.co.uk', 'www.si.com', 'img.tfd.com', 'cdn.mefi.us', 's.yimg.com'],
     remotePatterns: [
